5bd63a7e399904a25a1a42ca57e5422e8defc4a3
Victor Amarante <vafstats@gmail.com>
2024-11-06 14:30:24 -0300

    obtendo a moeda selecionada pelo usuario

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
 
 amount.addEventListener("input", (event) => {
   const hasCharactersRegex = /\D+/g
   let inputValue = amount.value
   inputValue = inputValue.replace(hasCharactersRegex, "")
   console.log(inputValue)
+})
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  console.log(currency.value)
 })
