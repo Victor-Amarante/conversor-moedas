b6e3480d1c7067905d62b95f8aa11c808525334c
Victor Amarante <vafstats@gmail.com>
2024-11-06 14:40:18 -0300

    identificando a moeda selecionada

--- scripts.js
+++ scripts.js
@@ -2,14 +2,31 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
 amount.addEventListener("input", (event) => {
   const hasCharactersRegex = /\D+/g
   let inputValue = amount.value
   inputValue = inputValue.replace(hasCharactersRegex, "")
-  console.log(inputValue)
 })
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
-  console.log(currency.value)
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+  }
 })
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
